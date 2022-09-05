5add09a38200ebfc34d6d2b6bbe4e051a0f6420f
wffchong <283953455@qq.com>
2022-09-05 14:31:43 +0800

    配置跨域及路径别名

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from 'axios'
+
+axios.post('/api/user_router_list', { uid: 2 }).then(res => {
+    console.log(res.data)
+})
+</script>
 
 <template></template>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue()],
+    // 配置路径别名
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src')
+        }
+    },
+    // 配置跨域
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8081',
+                changeOrigin: true,
+                rewrite: path => path.replace(/^\/api/, '')
+            }
+        }
+    }
 })
